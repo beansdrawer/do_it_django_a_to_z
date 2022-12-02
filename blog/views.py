54e59556280efd5dfdaa81f1262ee444faa258f9
beansdrawer <beansdrawer@naver.com>
2022-12-02 17:51:06 +0900

    로그인한 사용자만 글쓰기 가능

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post, Category, Tag
+
+# 믹스인 : 클래스를 상속하지 않고도 메소드를 조합할 수 있는 기법
+class PostCreate(LoginRequiredMixin, CreateView) :
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated :
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else :
+            print("redirect('/blog/')")
+            return redirect('/blog/')
 
 class PostList(ListView) :
     model = Post
@@ -23,6 +38,20 @@ class PostDetail(DetailView) :
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+def tag_page(request, slug) :
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count()
+        }
+    )
 
 def category_page(request, slug) :
 
