9a0f971d876bca9e2a03362de6dc7a79c7d94802
beansdrawer <beansdrawer@naver.com>
2022-12-05 10:38:26 +0900

    포스트 수정 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
+from django.core.exceptions import PermissionDenied
+
+class PostUpdate(LoginRequiredMixin, UpdateView) :
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author :
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else :
+            raise PermissionDenied
 
 # 믹스인 : 클래스를 상속하지 않고도 메소드를 조합할 수 있는 기법
-class PostCreate(LoginRequiredMixin, CreateView) :
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView) :
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
     def form_valid(self, form):
         current_user = self.request.user
-        if current_user.is_authenticated :
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser) :
             form.instance.author = current_user
             return super(PostCreate, self).form_valid(form)
         else :
