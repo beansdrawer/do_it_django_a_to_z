4e6b1e9867af0d3cb09a4345262f5a3f99a4cb36
beansdrawer <beansdrawer@naver.com>
2022-12-06 09:04:18 +0900

    16장까지는 마무리

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
 import os
+from markdownx.utils import markdown
 
 class Tag(models.Model) :
     name = models.CharField(max_length=50, unique=True)
 
     # slug : url을 생성하기 위해 문자를 조합하는 방식
     slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+    # slug = models.SlugField(max_length=50, allow_unicode=True)
 
     def __str__(self):
         return self.name
@@ -34,7 +37,8 @@ class Category(models.Model) :
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
-    content = models.TextField()
+    # content = models.TextField()
+    content = MarkdownxField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
@@ -60,4 +64,7 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+    def get_content_markdown(self):
+        return markdown(self.content)
 
--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'crispy_forms',
+    'markdownx',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'blog',
     'single_pages',
 ]
@@ -124,9 +131,21 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKEND = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticateionBackend',
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+LOGIN_REDIRECT_URL = '/blog/'
